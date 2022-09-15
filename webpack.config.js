623561aa35960a29fb790c7d72ef5a72f67d55ab
gladdos <lite.console4bit@gmail.com>
2022-09-15 18:30:06 -0400

    CI/CD

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const base = require('./config/webpack.base.js');
 const {merge} = require('webpack-merge');
 
 module.exports = (env) => {
-    switch (env.mode) {
+    switch (env.MODE) {
         case 'development':
-            return merge({mode: 'development', devtool: 'source-map'}, base.commonConfig())
+            return merge({MODE: 'development', devtool: 'source-map'}, base.commonConfig())
         case 'production':
-            return merge({mode: 'production'}, base.commonConfig())
+            return merge({MODE: 'production'}, base.commonConfig())
         default:
             throw new Error('No matching configuration was found!');
     }
