81228ec29d8f0e344ad94333262b47bb66847327
glad2os <lite.console4bit@gmail.com>
2022-10-20 13:01:45 -0400

    updating items

--- routes/business.js
+++ routes/business.js
@@ -8,7 +8,7 @@ router.get('/', async function (req, res) {
     if (await userDB.validateUserBySessionData(req.session.userid)) {
         res.render('index', {className: 'business', title: 'Express', page: "business", authenticated: req.session.userid});
     } else {
-        res.render('error', {message: '403 Forbidden Error', title: 'Express', page: "business"});
+       res.redirect('/login')
     }
 });
 
